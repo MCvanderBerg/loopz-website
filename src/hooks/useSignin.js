2699e4048b8512130728806cde870028e5bf2e8f
MCvanderBerg <christiaan26c@gmail.com>
2024-05-26 23:08:49 +0200

    fixed signup

--- src/hooks/useSignin.js
+++ src/hooks/useSignin.js
@@ -1,29 +1,33 @@
 import { useState } from "react"
 import { useAuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { Router } from "../types/Types"
 
-
-const useLogin = () => {
+const useSignin = () => {
     const [isLoading, setIsLoading] = useState(null);
     const [error, setError] = useState(null);
     const { dispatch }  = useAuthContext()
-
-    const login = async (email, password) => {
+    const navigate = useNavigate();
+    
+    const signin = async (obj) => {
         setIsLoading(true)
         setError(null)
 
 
-        const response = await fetch(`api/user/login`,{
-            method: "POST", 
+        const response = await fetch(`api/user/signup`,{
+            method: "POST",
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({email_address:email, password})
+            body: JSON.stringify(obj)
         })
 
         const json = await response.json()
-        console.log("response: ",json)
 
-        if (!response.ok) {
+        console.log("response: ", json)
+
+
+        if (!response.ok){
             setIsLoading(false)
             setError(json.error)
         }
@@ -31,11 +35,12 @@ const useLogin = () => {
         if (response.ok) {
             localStorage.setItem('session', JSON.stringify(json))
             dispatch({ type: "LOGIN", payload: json })
+            navigate(Router.events)
             setIsLoading(false)
         }
     }
 
-    return { login, isLoading, error }
+    return { signin, isLoading, error }
 }
 
-export default useLogin
+export default useSignin
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,15 +1,26 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Router } from "../types/Types";
+import useSignin from "../hooks/useSignin"
 
 const SignupScreen = () => {
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+
+  const { signin, isLoading, error } = useSignin()
+
+
   const navigate = useNavigate();
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password);
+
+    console.log({email_address:email, password, username, name, surname, phone_number: phoneNumber});
+    signin({email_address:email, password, username, name, surname, phone_number: phoneNumber})
   };
 
   const goToLogin = () => {
@@ -24,6 +35,24 @@ const SignupScreen = () => {
     setPassword(e.target.value);
   };
 
+  const changeUsername = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const changeName = (e) => {
+    setName(e.target.value)
+  }
+
+  const changeSurname = (e) => {
+    setSurname(e.target.value)
+  }
+
+  const changePhoneNumber = (e) => {
+    setPhoneNumber(e.target.value)
+  }
+
+
+
   return (
     <form className="signup" onSubmit={onSubmit}>
       <h3>Signup</h3>
@@ -32,13 +61,13 @@ const SignupScreen = () => {
       <label>Password</label>
       <input type="text" onChange={changePassword} value={password} />
       <label>UserName</label>
-      <input type="text" onChange={changePassword} value={password} />
+      <input type="text" onChange={changeUsername} value={username} />
       <label>Name</label>
-      <input type="text" onChange={changePassword} value={password} />
+      <input type="text" onChange={changeName} value={name} />
       <label>Surname</label>
-      <input type="text" onChange={changePassword} value={password} />
+      <input type="text" onChange={changeSurname} value={surname} />
       <label>Phone number</label>
-      <input type="text" onChange={changePassword} value={password} />
+      <input type="text" onChange={changePhoneNumber} value={phoneNumber} />
       <div>
         <button type="button" onClick={goToLogin}>
           Go to login
